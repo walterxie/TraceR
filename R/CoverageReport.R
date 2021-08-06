77ff2aaa58959f39e8fdd93e1184e6273ed74094
walterxie <walterxie@users.noreply.github.com>
2021-08-06 14:26:13 +1200

    start coverage pipeline

--- R/CoverageReport.R
+++ R/CoverageReport.R
@@ -7,137 +7,128 @@
 #' @description
 #' The pipeline to generate a coverage report of the model validation.
 #'
-#' @details
-#' \code{summariseTracesAndTrees} creates the statistical summary of
-#' traces from a BEAST log file and tree statistics from BEAST tree log file,
-#' and save them into tsv files.
+#' 1. Summarise traces statistics for every simulations (normally starting 110,
+#'    where 10 extra simulations are used for replacing any low ESS results
+#'    in the next step), and save each result into a tsv file.
+#'    Note: if tree stats are logged into BEAST log file,
+#'    you can set \code{tree.file=NA} to skip creating tree stats tsv file.
+#'    See \code{\link{summariseTracesAndTrees}}.
 #'
-#' @param log.file  The BEAST log file containing posterior samples.
-#' @param tree.file The BEAST tree log file containing posterior trees.
-#'                  Default to NA, which will skip to report the tree statistics.
-#' @param burn.in   The proportion of samples treated as the burn-in stage
-#'                  during MCMC. Default to 0.1.
-#' @param stats.fn.fun,tree.stats.fn.fun The function to create the result file names
-#'                  from the given log file names. Set to NA, if you do not want to
-#'                  create the files.
+#' 2. Select 100 simulations where the ESS of every parameters are guaranteed
+#'    >= 200. If not, then replace it to the one from the extra 10 sequentially,
+#'    and check ESS. If all extra 10 are used but there exists any low-ESS simulations,
+#'    then the pipeline stops and inform to re-run all simulations with longer
+#'    MCMC chain length. See \code{\link{selectResultByESS}}.
+#'
+#' 3. Create a final summary file for visualisations.
+#'    See \code{\link{summariseParameters}} and \code{\link{summariseTrueValues}}.
+#'
+#' 4. Plot coverage.
+#'
+#'
+#' @details
+#' \code{selectResultByESS} selects 100 simulation results, where the ESS of
+#' every parameters are guaranteed >= 200.
+#' The inputs files are produced by \code{\link{summariseTracesAndTrees}}.
+#' Set \code{tree.file.postfix=NA} and \code{extr.tree.files=c()},
+#' if it does not require tree stats tsv files.
+#'
+#' @param i.sta,i.end  The start index or end index of batch runs,
+#'                     also used in the file names.
+#' @param prefix  The prefix of statistics summary files.
+#' @param file.postfix,tree.file.postfix
+#'                The extension of statistics summary files.
+#'                This function uses \code{\link{readTracesTSV}},
+#'                The file extension can be different, but the file
+#'                has to be tab-delimited text file.
+#' @param file.steam.fun,extra.file.steam.fun
+#'                The function to extract the file steam,
+#'                which is used as a key to identify the simulation.
+#' @param extra.files,extr.tree.files
+#'                The extra files used to replace the low ESS results.
 #' @keywords Coverage
 #' @export
 #' @examples
 #' WD = file.path("~/WorkSpace/MyValidations/")
 #' setwd(WD)
-#' log.files = list.files(pattern = ".log")
-#' for(lg in log.files) {
-#'   # assume same file stem
-#'   tree.file=paste0(sub('\\.log$', '', lg), ".trees")
-#'   res <- summariseTracesAndTrees(lg, tree.file)
-#' }
+#' all.stats = list.files(pattern = "_([0-9]+).tsv")
+#' all.stats # a char vector
+#' extra.stats = all.stats[grep("_10([0-9]).tsv", all.stats, ignore.case = T)]
+#' extra.stats # extra 10 simulations: *_100.tsv ~ *_109.tsv
 #'
-#' # skip tree logs
-#' summariseTracesAndTrees(lg, tree.file=NA)
+#' # skip tree stats
+#' sele.res <- selectResultByESS(i.sta=0, i.end=99, prefix="sim",
+#'                               tree.file.postfix=NA,
+#'                               extra.files = extra.stats)
+#' low.ess <- sele.res$lowESS
+#' sele.list <- sele.res$selected
+#' names(sele.list)
 #'
 #' @rdname Coverage
-summariseTracesAndTrees <- function(log.file, tree.file=NA, burn.in=0.1,
-                                    stats.fn.fun=function(x){ paste0(sub('\\.log$', '', x), ".tsv") },
-                                    tree.stats.fn.fun=function(x){ paste0(x, ".tsv") }) {
-  require(tidyverse)
-
-  cat("\nProcess ", log.file, "...\n")
-  if (!file.exists(log.file)) stop("Cannot find file : ", log.file)
-
-  # read MCMC log
-  mcmc.log <- readMCMCLog(log.file)
-  # get traces and remove burn in
-  traces <- getTraces(mcmc.log, burn.in=burn.in)
-  # get stats
-  stats <- analyseTraces(traces)
-
-  if (!is.function(stats.fn.fun)) write_tsv(stats, stats.fn.fun(log.file))
-
-  # add tree stats
-  tre.sta <- NULL
-  if (!is.na(tree.file)) {
-    if (!file.exists(tree.file)) stop("Cannot find file : ", tree.file)
-
-    tre.sta.df <- readTrees(tree.file)
-    tre.sta <- analyseTreeStats(tre.sta.df)
-
-    # ? HPD95.lower.STATE_14150000
-    #tre.stats$trace <- sub("\\.STATE.*$","",tre.stats$trace, ignore.case = T)
-    if (!is.function(tree.stats.fn.fun))
-      write_tsv(tre.sta, tree.stats.fn.fun(tree.file))
-  }
-  list(stats=stats, tree.stats=tre.sta)
-}
-
-
 selectResultByESS <- function(i.sta=0, i.end=99, prefix="sim",
-                              file.steam.fun=function(prefix, i){ paste0(prefix, "_", i) }
+                              file.steam.fun=function(prefix, i){ paste0(prefix, "_", i) },
                               file.postfix="tsv", tree.file.postfix="trees.tsv",
-                              extra.files = c() ) {
+                              extra.file.steam.fun=function(f){ sub('\\.tsv$', '', f) },
+                              extra.files = c(), extr.tree.files=c() ) {
   require(tidyverse)
   # record 100 files whose ESS >= 200
   tracesDF <- list()
   # if any of 100 has <200 ESS, then it will be replaced by one of extra 10
-  lowESS <- tibble()
-  etr <- 1
+  lowESS <- tibble(origin=character(),minESS=numeric(),replace=character())
+  extr <- 1
   for(i in i.sta:i.end) {
     fn <- file.steam.fun(prefix, i)
     fi <- paste0(fn, ".", file.postfix)
-    stopifnot(file.exists(fi))
-
     cat("Load ", fi, "...\n")
-    # "trace" col has "mean", ..., "HPD95.lower", "HPD95.upper", "ESS"
-    traces <- try(read_tsv(fi))
-    # take ESS row
-    ESS <- traces %>% filter(trace == "ESS") %>% select(!trace)
-    minESS <- min(ESS %>% as.numeric)
-    cat(fi, ", min ESS = ", minESS, "\n")
 
-    # if tree stats file exists
-    tre.sta.fi <- paste0(fn, ".", tree.file.postfix)
-    if (file.exists(tre.sta.fi)) {
-      # add tree stats
-      traces <- addTreeStats(tre.sta.fi, traces)
+    res <- readTracesTSV(traces.file=fi)
+    minESS <- res$minESS
+    traces <- res$traces
+
+    if (!is.na(tree.file.postfix)) {
+      # if tree stats file exists
+      tre.sta.fi <- paste0(fn, ".", tree.file.postfix)
+      if (file.exists(tre.sta.fi)) {
+        cat("Load ", tre.sta.fi, "...\n")
+        # add tree stats (in cols)
+        traces <- addTreeStats(tre.sta.fi, traces)
+      }
     }
 
-
-
-
-    #TODO
+    # if low ESS
     if (minESS < 200) {
-      selected <- pullAnthorFromExtra(etr, extra.files=extra.files)
+      # skip tree stats, if length(extr.tree.files)<=0
+      selected <- pullAnthorFromExtra(extr,
+                                      extra.file.steam.fun=extra.file.steam.fun,
+                                      extra.files=extra.files,
+                                      extr.tree.files=extr.tree.files )
       # selected extra
       file.selected <- selected$file.selected
       cat("Replace the low ESS result", fi, "to", file.selected, "\n")
 
-      fn.sel <- sub('\\.tsv$', '', file.selected)
+      fn.sel <- extra.file.steam.fun(file.selected)
       tracesDF[[fn.sel]] <- selected$traces
 
       # sync current index in the pool
-      etr <- selected$extr.idx
+      extr <- selected$curr.idx
 
       # record low ESS
-      tmp.low <- ESS %>% as_tibble %>% add_column(fn, .before=1) %>% add_column(fn.sel, .before=2)
-      lowESS <- lowESS %>% rbind(tmp.low)
+      lowESS <- lowESS %>% add_row(origin=fn,minESS=minESS,replace=fn.sel)
+      if (nrow(selected$minESS)>0) {
+        lowESS <- lowESS %>% rbind(selected$minESS)
+      }
 
     } else {
       tracesDF[[fn]] <- traces
     }
-  }
+  } # end for loop
 
   list(selected = tracesDF, lowESS = lowESS)
 }
 
-
-
-
-
-
-
-
 ### private functions
 
-# add tree stats DF into traces DF
+# add tree stats DF (cols) into traces DF
 addTreeStats <- function(tree.stats.file="sim_0.trees.tsv", traces) {
   # add tree stats
   tre.sta <- try(read_tsv(tre.sta.fi)) %>% select(!trace)
@@ -148,54 +139,112 @@ addTreeStats <- function(tree.stats.file="sim_0.trees.tsv", traces) {
   return( traces %>% left_join(tre.sta, by = "trace") )
 }
 
+#sim_0.tsv
 #trace	posterior	likelihood
 #mean	-1395.71800741416	-1260.57129157319
 #stderr.of.mean	0.948421280126143	0.142003271885284
-#stdev	21.6784291082163	5.61509246716363
-pullAnthorFromExtra <- function(extr.idx, extra.files=c(), extr.tree.files=c()) {
+pullAnthorFromExtra <- function(curr.idx,
+                                extra.file.steam.fun=function(f){ sub('\\.tsv$', '', f) },
+                                extra.files=c(), extr.tree.files=c()) {
   require(tidyverse)
-  if (extr.idx >= length(extra.files)) # length(extra.files) extra
-    stop("All extra simulations have been used ! ", extr.idx, " >= ", length(extra.files))
+  if (curr.idx >= length(extra.files)) # length(extra.files) extra
+    stop("Not enough extra simulations to replace low-ESS simulations !\n",
+         "Please re-run all simulations with longer MCMC chain length.\n",
+         curr.idx, " >= ", length(extra.files))
 
-  minESS.list <- list()
+  lowESS <- tibble(origin=character(),minESS=numeric(),replace=character())
 
-  while (extr.idx < length(extra.files)) {
-    # pick up "extr.idx" result from extra pool
-    et.fi <- extra.files[extr.idx]
-    stopifnot(file.exists(et.fi))
+  while (curr.idx < length(extra.files)) {
+    # pick up "curr.idx" result from extra pool
+    et.fi <- extra.files[curr.idx]
 
     # must incl trace column
-    traces <- try(read_tsv(et.fi))
-
-    ESS <- traces %>% filter(trace == "ESS") %>%
-      select(!trace) %>% as.numeric
-    minESS <- min(ESS)
-    minESS.list[[et.fi]] <- minESS
-    cat(et.fi, ", min ESS = ", minESS, "\n")
+    res <- readTracesTSV(traces.file=et.fi)
+    minESS <- res$minESS
+    traces <- res$traces
 
     # all ESS >= 200 then break, otherwise continue the loop
     if (minESS >= 200) {
-
       if (length(extr.tree.files)>0) {
-        tre.sta.fi <- extr.tree.files[extr.idx]
+        tre.sta.fi <- extr.tree.files[curr.idx]
         stopifnot(file.exists(tre.sta.fi))
         # add tree stats
         traces <- addTreeStats(tre.sta.fi, traces)
       }
       # point to the next before break loop
-      extr.idx <- extr.idx + 1
+      curr.idx <- curr.idx + 1
       break
     } # end if minESS >= 200
 
+    # record low ESS
+    fn.extra <- extra.file.steam.fun(et.fi)
+    lowESS <- lowESS %>% add_row(origin=fn.extra,minESS=minESS,replace="")
+
     # must increase after pick up tree stats from extra pool
-    extr.idx <- extr.idx + 1
+    curr.idx <- curr.idx + 1
   } # end while
 
-  list(file.selected=et.fi, extr.idx=extr.idx,
-       traces=traces, minESS.list = minESS.list )
+  list(file.selected=et.fi, curr.idx=curr.idx, traces=traces,
+       minESS = minESS, lowESS = lowESS )
 }
+### end of private functions
+
+#' @details
+#' \code{summariseParameters} creates a final summary file for each given
+#' parameters.
+#'
+#' @param selected   The list of data frames containing traces summary,
+#'                   produced by \code{\link{selectResultByESS}}.
+#' @param params     The vector of parameter names in the BEAST log files.
+#' @param stats.name The vector of names of statistics. They have to be one of
+#'                   names from the 1st column 'trace' in *.tsv file.
+#' @keywords Coverage
+#' @export
+#' @examples
+#' WD = file.path("~/WorkSpace/MyValidations/")
+#' setwd(WD)
+#' true.logs = list.files(pattern = "_true.log")
+#' true.trees = list.files(pattern = "_true_ψ.trees")
+#' summariseTrueValues()
+#'
+#' @rdname Coverage
+summariseParameters <- function(selected=list(),
+          params = c("mu","Theta", "kappa", "psi.treeLength", "psi.height"),
+          stats.name = c("mean", "HPD95.lower", "HPD95.upper", "stderr.of.mean", "ESS")) {
+  require(tidyverse)
+  cat("Select ", length(selected)," simulations : ", paste(names(selected), collapse = ", "), ".\n")
+
+  minESS <- c()
+
+  for (pa in params) {
+    cat("Analyse parameter : ", pa, "...\n")
+    df <- tibble(trace=stats.name)
+
+    tra.par <- NULL
+    for(i in 1:length(selected)) {
+      # "mean", "HPD95.lower", "HPD95.upper", "ESS"
+      tra.par <- selected[[i]] %>% select(trace, pa) %>% unlist
+      stopifnot(length(tra.par) > 0)
+
+      df <- try(df %>% add_column(!!(names(selected)[i]) := tra.par))
+    }
+
+    ESS <- df %>% filter(trace == "ESS") %>% select(!trace) %>% unlist
+    # to numeric
+    tmp.minESS <- min(ESS %>% as.numeric)
+    cat("min ESS = ", tmp.minESS, "\n")
+    minESS <- c(minESS, tmp.minESS)
+
+    if (!is.na(tmp.minESS) && tmp.minESS >= 200)
+      write_tsv(df, paste0(pa, ".tsv"))
+    else
+      warning("Summary not generated ! ", pa, " min ESS = ", tmp.minESS, "\n")
+  }
+  cat("min ESS = ", paste(minESS, collapse = ", "), "\n")
+  cat("min of min ESS = ", min(minESS), "\n")
 
 
+}
 
 #' @details
 #' \code{summariseTrueValues} creates one file to contain all of the true values
---
+++ R/TracerAdv.R
@@ -0,0 +1,102 @@
+# Author: Walter Xie
+# Accessed on 6 Aug 2021
+
+#' @name TracerAdv
+#' @title The Pipeline Functions to Summarise Bayesian MCMC Results
+#'
+#' @description
+#' The several integrated functions based on \code{\link{Tracer}} and
+#' \code{\link{TreeStats}}, which are useful to create a pipeline to
+#' summarise groups of batch runs.
+#'
+#'
+#' @details
+#' \code{summariseTracesAndTrees} creates the statistical summary of
+#' traces from a BEAST log file and tree statistics from BEAST tree log file,
+#' and save them into tsv files.
+#'
+#' @param log.file  The BEAST log file containing posterior samples.
+#' @param tree.file The BEAST tree log file containing posterior trees.
+#'                  Default to NA, which will skip to report the tree statistics.
+#' @param burn.in   The proportion of samples treated as the burn-in stage
+#'                  during MCMC. Default to 0.1.
+#' @param stats.fn.fun,tree.stats.fn.fun The function to create the result file names
+#'                  from the given log file names. Set to NA, if you do not want to
+#'                  create the files.
+#' @keywords TracerAdv
+#' @export
+#' @examples
+#' WD = file.path("~/WorkSpace/MyValidations/")
+#' setwd(WD)
+#' log.files = list.files(pattern = "_([0-9]+).log")
+#' log.files # exclude _true.log
+#' for(lg in log.files) {
+#'   # assume same file stem
+#'   tree.file=paste0(sub('\\.log$', '', lg), ".trees")
+#'   res <- summariseTracesAndTrees(lg, tree.file)
+#' }
+#'
+#' # skip tree logs
+#' summariseTracesAndTrees(log.file="sim_0.log", tree.file=NA)
+#'
+#' @rdname TracerAdv
+summariseTracesAndTrees <- function(log.file, tree.file=NA, burn.in=0.1,
+                                    stats.fn.fun=function(x){ paste0(sub('\\.log$', '', x), ".tsv") },
+                                    tree.stats.fn.fun=function(x){ paste0(x, ".tsv") }) {
+  require(tidyverse)
+
+  cat("\nProcess ", log.file, "...\n")
+  if (!file.exists(log.file)) stop("Cannot find file : ", log.file)
+
+  # read MCMC log
+  mcmc.log <- readMCMCLog(log.file)
+  # get traces and remove burn in
+  traces <- getTraces(mcmc.log, burn.in=burn.in)
+  # get stats
+  stats <- analyseTraces(traces)
+
+  if (!is.function(stats.fn.fun)) write_tsv(stats, stats.fn.fun(log.file))
+
+  # add tree stats
+  tre.sta <- NULL
+  if (!is.na(tree.file)) {
+    if (!file.exists(tree.file)) stop("Cannot find file : ", tree.file)
+
+    tre.sta.df <- readTrees(tree.file, burn.in=burn.in)
+    tre.sta <- analyseTreeStats(tre.sta.df)
+
+    # ? HPD95.lower.STATE_14150000
+    #tre.stats$trace <- sub("\\.STATE.*$","",tre.stats$trace, ignore.case = T)
+    if (!is.function(tree.stats.fn.fun))
+      write_tsv(tre.sta, tree.stats.fn.fun(tree.file))
+  }
+  list(stats=stats, tree.stats=tre.sta)
+}
+
+#' @details
+#' \code{readTracesTSV} loads traces tsv format file into a data frame,
+#' and report the minimum ESS.
+#' @seealso \code{\link{read_tsv}}
+#'
+#' @param traces.file  The tsv file containing traces statistics.
+#' @keywords TracerAdv
+#' @export
+#' @examples
+#' res <- readTracesTSV(traces.file="sim_0.tsv")
+#' # res$minESS
+#' traces <- res$traces
+#'
+#' @rdname TracerAdv
+readTracesTSV <- function(traces.file="sim_0.tsv", ...) {
+  require(tidyverse)
+  stopifnot(file.exists(traces.file))
+  # "trace" col has "mean", ..., "HPD95.lower", "HPD95.upper", "ESS"
+  traces <- try(read_tsv(traces.file, ...))
+  # take ESS row
+  ESS <- traces %>% filter(trace == "ESS") %>% select(!trace)
+  minESS <- min(ESS %>% as.numeric)
+  cat(traces.file, " has min ESS ", minESS, ".\n")
+  list(traces=traces, minESS=minESS)
+}
+
+
