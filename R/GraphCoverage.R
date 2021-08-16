f24415430b58c2dd7cf56210eed59fccd725e47d
walterxie <walter@cs.auckland.ac.nz>
2021-08-16 15:27:26 +1200

    add data saturation

--- R/GraphCoverage.R
+++ R/GraphCoverage.R
@@ -34,13 +34,14 @@
 #'                    by minimum and maximum values. They are only used
 #'                    for log scale.
 #' @param x.txt.just Where the text "covg. =" starts, default to 0.
-#' @keywords Tracer
+#' @keywords Coverage
 #' @export
 #' @examples
 #' param = "mu"
 #' df.pos <- read_tsv(paste0(param, ".tsv"))
 #' df.tru <- read_tsv("trueValue.tsv")
 #' inOut <- markInOut(df.pos, df.tru, tru.val.par="μ")
+#' write_tsv(inOut, paste0(param, "-coverage.tsv"))
 #' # coverage
 #' cov <- nrow(subset(inOut, is.in==TRUE)) / nrow(inOut)
 #' p <- ggCoverage(inOut, x.lab=paste("True",param,"value"))
@@ -51,7 +52,6 @@
 #' nrow(df.sub)
 #' cov.per <- round(cov * 100)
 #' p <- ggCoverage(df.sub, cov.per, x.lab=paste("True",param,"value"))
-#' ggsave(paste0(param, "-sub-",nrow(df.sub),".pdf"), p, width = 4, height = 3)
 #'
 #' @rdname CovgGraph
 ggCoverage <- function(df, cov.per=-1, transp = 0.3, x.lab="", y.lab="Mean posterior",
@@ -90,4 +90,74 @@ ggCoverage <- function(df, cov.per=-1, transp = 0.3, x.lab="", y.lab="Mean poste
   return(p)
 }
 
+#' @details
+#' The data saturation in the simluated data may cause MCMC defficult to converge.
+#' \code{ggDataSaturation} returns a \code{\link{ggplot}} object to visualise
+#' the maximum distances of overall partitions \code{max(root height * mu * r)}.
+#' It is also coloured by the coverages.
+#'
+#' @param gene.dist The data frame contains simulation and distances.
+#' @param x.col,y.col,colour.col The column names.
+#' @param x.lab,y.lab   The label of x-axis or y-axis".
+#' @keywords Coverage
+#' @export
+#' @examples
+#'
+#' ### create data frame for data saturation visualisation
+#'
+#' df.mu <- read_tsv("mu-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "true.val") %>% rename(mu=true.val)
+#' df.r0 <- read_tsv("r_0-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "true.val") %>% rename(r0=true.val)
+#' df.r1 <- read_tsv("r_1-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "true.val") %>% rename(r1=true.val)
+#' df.r2 <- read_tsv("r_2-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "true.val") %>% rename(r2=true.val)
+#' df.hei <- read_tsv("psi.height-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "true.val") %>% rename(height=true.val)
+#'
+#' df.colour <- read_tsv("Theta-coverage.tsv", col_types = cols()) %>%
+#'   select("simulation", "is.in")
+#'
+#' gene.dist <- df.mu %>% inner_join(df.hei, by = "simulation") %>%
+#'   inner_join(df.r0, by = "simulation") %>%
+#'   inner_join(df.r1, by = "simulation") %>%
+#'   inner_join(df.r2, by = "simulation") %>%
+#'   mutate(max.r = pmax(r0, r1, r2)) %>%
+#'   mutate(distance = height*mu*max.r) %>%
+#'   select(-r0, -r1, -r2) %>%
+#'   inner_join(df.colour, by = "simulation")
+#'
+#' gene.dist
+#' #   simulation  mu     height max.r  distance is.in
+#' #   <chr>       <dbl>  <dbl>  <dbl>   <dbl>   <lgl>
+#' # 1 al2_0      0.0165   72.0  1.36    1.62    TRUE
+#' # 2 al2_1      0.00145  158.  1.99    0.458   TRUE
+#'
+#' p <- ggDataSaturation(gene.dist)
+#' @rdname CovgGraph
+ggDataSaturation <- function(gene.dist, x.col = "simulation", y.col = "distance",
+                             colour.col = "is.in", x.lab="", y.lab="max(root height * mu * r)") {
+
+  require("tidyverse")
+  require("ggplot2")
+
+  stopifnot( any(c(x,y,colour) %in% colnames(gene.dist)) )
+
+  p <- ggplot(gene.dist, aes(x = !!sym(x.col), y = !!sym(y.col),
+                             group = !!sym(colour.col),
+                             colour = !!sym(colour.col)) ) +
+    geom_point(aes(shape = !!sym(colour.col)), size = 0.2, alpha = 0.9) +
+    scale_shape_manual(values=c(4, 1, 0))+
+    geom_hline(yintercept = 1.0, linetype = "dotted", size = 0.2) +
+    geom_hline(yintercept = 0.5, linetype = "dotted", size = 0.2) +
+    scale_y_log10() +
+    xlab(x.lab) + ylab(y.lab) + guides(colour = FALSE, shape = FALSE) +
+    theme_classic() +
+    theme(text = element_text(size = 15),
+          axis.text.x=element_blank(),
+          axis.ticks.x=element_blank())
+
+  return(p)
+}
 
